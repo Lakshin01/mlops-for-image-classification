cea5accaaad16f0aa551b809f009036b6b9531d2
EC2 Default User <ec2-user@ip-172-31-90-210.ec2.internal>
2022-10-24 06:06:26 +0000

    ci setup for devops

---
+++ main.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    """ Add 2 numbers """
+    
+    result = int(a) + int(b)
+    return result
+
+
+print(add(14, 3))
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import add
+
+def test_add():
+    assert 17 == add(14,3)
